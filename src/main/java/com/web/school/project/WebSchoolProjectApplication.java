c3764fd719c8e8540d89c2aec6865d80207b66a6
Maximiliano-G17 <maxigimenez1792@gmail.com>
2021-02-26 11:25:27 -0300

    first commit

---
+++ src/main/java/com/web/school/project/WebSchoolProjectApplication.java
@@ -0,0 +1,13 @@
+package com.web.school.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSchoolProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSchoolProjectApplication.class, args);
+	}
+
+}
