e9e26f0efca80cd3ef154a8246ce3bbda2369371
Maximiliano-G17 <maxigimenez1792@gmail.com>
2021-03-06 13:31:37 -0300

    adding methods to search students by teacherId and search subjects by teacherId and using it in html

--- src/main/java/com/web/school/project/controller/TeacherController.java
+++ src/main/java/com/web/school/project/controller/TeacherController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.web.school.project.domain.Student;
+import com.web.school.project.domain.Subject;
 import com.web.school.project.domain.Teacher;
 import com.web.school.project.service.TeacherService;
 
@@ -23,5 +26,23 @@ public class TeacherController {
 		
 		model.addAttribute("findAll", findAll);
 		return "views/teachers";
+	}
+	
+	@RequestMapping("/students/{id}")
+	public String readAllStudentsByTeacherId(@PathVariable Long id, Model model) {
+		List<Student> allStudents = teacherService.findStudentsByTeacherId(id);
+		
+		model.addAttribute("findAll", allStudents);
+		
+		return "views/students";
+	}
+	
+	@RequestMapping("/subjects/{id}")
+	public String readAllSubjectsByTeacherId(@PathVariable Long id, Model model) {
+		List<Subject> allSubjects = teacherService.findSubjectsByTeacherId(id);
+		
+		model.addAttribute("findAll", allSubjects);
+		
+		return "views/subjects";
 	}
 }
